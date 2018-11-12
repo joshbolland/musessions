cc65d67621860e271eadcb6b708f8e1b88fe2d62
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2018-11-12 18:08:48 +0000

    Created controller and views for Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
