b4a43307d4f69ba2fa017c5d58ad509f0324f63d
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2018-11-12 18:02:10 +0000

    Created bookings model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :booked_musicians, through: :bookings, source: :user
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_155701) do
+ActiveRecord::Schema.define(version: 2018_11_12_174506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "job_description"
+    t.string "location"
+    t.datetime "job_date"
+    t.bigint "booked_musician_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booked_musician_id"], name: "index_bookings_on_booked_musician_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +45,6 @@ ActiveRecord::Schema.define(version: 2018_11_12_155701) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "users"
+  add_foreign_key "bookings", "users", column: "booked_musician_id"
 end
