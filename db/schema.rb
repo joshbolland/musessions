2c8715c353137f8a63e8ddfc706fc0d30fb1a3b5
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2018-11-12 16:07:04 +0000

    Added columns to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_151355) do
+ActiveRecord::Schema.define(version: 2018_11_12_155701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2018_11_12_151355) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.boolean "is_musician", default: false
+    t.string "location"
+    t.integer "rate"
+    t.string "instrument"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
