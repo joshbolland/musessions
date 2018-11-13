3ebcd035789291948ec1d09f57de1f492870a875
Renata Svaboviciute <renata.svaboviciute@gmail.com>
2018-11-13 12:18:28 +0000

    Created Bookings actions for new and create. Created view for Bookings new.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,11 +6,30 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @musician = User.find(params[:user_id])
+    @booker = current_user
+    @booking = Booking.new
   end
 
   def create
+    @musician = User.find(params[:user_id])
+    @booker = current_user
+    @booking = Booking.new(booking_params)
+    @booking.user = @booker
+    @booking.booked_musician_id = @musician.id
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:job_description, :location, :job_date)
   end
 end
