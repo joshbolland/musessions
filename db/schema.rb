3ad045c914e8a8e3ef3f1764ae607d676c8effde
Kaif Ahmed <hello@kaif.me>
2018-11-13 14:25:37 +0000

    Created a new migration to add in status column to booking table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_174506) do
+ActiveRecord::Schema.define(version: 2018_11_13_135029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2018_11_12_174506) do
     t.bigint "booked_musician_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
     t.index ["booked_musician_id"], name: "index_bookings_on_booked_musician_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
