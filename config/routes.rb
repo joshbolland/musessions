6aadc0d720162ff0245efe5fa1036e8a8dc74117
Kaif Ahmed <hello@kaif.me>
2018-11-14 13:15:30 +0000

    Implemented a UserInstrument controller, view and action

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :users, only: [:index, :show] do
     resources :bookings, only: [:new, :create]
-    resources :user_instrument, only: [:new, :create]
   end
   resources :bookings, only: [:index, :show, :destroy]
+  resources :user_instrument, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
